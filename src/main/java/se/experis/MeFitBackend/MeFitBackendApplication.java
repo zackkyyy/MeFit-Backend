ce7d086629bb651b90dff06cca9856dad036ef77
Zacky <zacky-kh@live.com>
2019-11-04 14:14:03 +0100

    Spring project initiated

---
+++ src/main/java/se/experis/MeFitBackend/MeFitBackendApplication.java
@@ -0,0 +1,13 @@
+package se.experis.MeFitBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeFitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeFitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/experis/MeFitBackend/MeFitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.MeFitBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeFitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
